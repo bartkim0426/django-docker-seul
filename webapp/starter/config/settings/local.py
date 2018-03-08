cf60c856d7e974f9a5b19eb84a8c7ce74583b22b
bartkim0426 <bartkim0426@gmail.com>
2018-03-08 12:06:46 +0900

    UPDATE : for dev env and django settings
    Files:
    *
    
    Why:
    *
    
    ETC:
    *

--- webapp/starter/config/settings/local.py
+++ webapp/starter/config/settings/local.py
@@ -17,10 +17,24 @@ INTERNAL_IPS = ['127.0.0.1', ]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ["POSTGRES_DB_NAME"],
+        'NAME': os.environ['POSTGRES_NAME'],
         'USER': os.environ["POSTGRES_USER"],
         'PASSWORD': os.environ["POSTGRES_PASSWORD"],
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'HOST': os.environ["POSTGRES_HOST"],
+        'PORT': os.environ["POSTGRES_PORT"],
     }
 }
+
+MEDIA_ROOT = str(ROOT_DIR('mediafiles'))
+# before collectstatic
+# for prevent duplication of STATIC_ROOT and STATICFILES_DIRS
+# STATIC_ROOT = str(ROOT_DIR('staticfiles'))
+# STATICFILES_DIRS = (
+#     str(ROOT_DIR.path('static')),
+# )
+
+# after collectstatic
+STATIC_ROOT = str(ROOT_DIR('static-files'))
+STATICFILES_DIRS = (
+    str(ROOT_DIR.path('staticfiles')),
+)
--- webapp/starter/config/settings/partials/STATIC.py
+++ webapp/starter/config/settings/partials/STATIC.py
@@ -2,10 +2,11 @@ import os
 
 from .BASE import ROOT_DIR, CONFIG_DIR
 
+# for integrate all static to staticfiles after migration
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(ROOT_DIR, 'staticfiles')
 STATIC_ROOT = str(ROOT_DIR('staticfiles'))
 # STATIC_ROOT = '/srv/static-files'
 STATICFILES_DIRS = (
-    str(ROOT_DIR.path('static')),
+    str(ROOT_DIR.path('staticfiles')),
 )
